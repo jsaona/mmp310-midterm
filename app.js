341145290d8992488854a0184ad45d7aa9952da5
joshpita <joshua.pita@yahoo.com>
2018-04-18 18:43:29 -0400

    update files

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 // document ready event
 $(document).ready(function() {
-	$('#query').on("keypress", function(event) {
+	$('.query').on("keypress", function(event) {
 		if (event.which == 13) {
-			var offset = Math.round(Math.random() * 1000);
-			var query = this.value;
 			var key = "6MXMy6ZYATEAV1L1DDsONAb002dfnMh2";
-			var url = "https://api.giphy.com/v1/gifs/search?q="
+			var query = this.value;
+			var animal = this.id;
+			var url = "https://api.giphy.com/v1/gifs/random?tag="
 						+ query
 						+ "&api_key="
-						+ key
-						+ "&limit=1"
-						+ "&offset="
-						+ offset;
+						+ key;
+			var animalUrl = "https://api.giphy.com/v1/gifs/random?tag="
+						+ animal
+						+ "&api_key="
+						+ key;
 
 			$.getJSON(url, function(json) {
-				console.log(json);
-				document.getElementById('gifs').innerHTML = "";
-				for (let i = 0; i < json.data.length; i++) {
-					const img = json.data[i];
-					if (img.images.downsized.url) {
-						const imgElem = $('<img>')
-							.attr('src', img.images.downsized.url);
+				$.getJSON(animalUrl, function(animalJson) {
+						console.log(json);
+					//document.getElementById('gifs').innerHTML = "";
+						if (json.data.images.downsized.url) {
 
-						const imgContainer = $('<div>')
-							.addClass('gif');
+							const imgElem = $('<img>')
+								.attr('src', json.data.images.downsized.url);
+							const imgContainer = $('<div>')
+								.addClass('gif');
+							imgContainer.append(imgElem);
 
-						imgContainer.append(imgElem);
-
-						 $('#gifs').append(imgContainer);
-					}
-				}
+							 $('#gifs').append(imgContainer);
+						}
+					});
 			});
 		}
 	});
